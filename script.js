0ed0cbf5e40470d7148e68777f0ab98e6dd2713e
mustafah rahimi <mustafahrahimii@gmail.com>
2025-10-30 03:41:31 +0100

    Putting the first bricks in place.

---
+++ script.js
@@ -0,0 +1,13 @@
+function openFile(filePath) {
+    window.open(filePath, '_blank');
+}
+
+function downloadResume() {
+    const filePath = 'docs/CV.pdf';
+    const link = document.createElement('a');
+    link.href = filePath;
+    link.download = 'Mustafah Rahimi CV.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
